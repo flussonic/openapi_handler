13cfe2b95389fa2cad49bdefeaf84b71945c711c
o.suvorova <o.suvorova@erlyvideo.org>
2023-06-02 13:04:03 +0300

    Add ability to receive response format in response headers #29339

--- test/test_schema_res.erl
+++ test/test_schema_res.erl
@@ -7,3 +7,16 @@ authorize(_) ->
 jsonArray(#{json_body := [1,2,3]} = Req) ->
   ct:pal("Req: ~p", [Req]),
   #{json_res => <<"1">>}.
+
+
+headersContentType(#{accept := _Accept, content_type := ContentType} = Req) ->
+  Content = #{
+    'text/plain' => <<"OK">>,
+    'application/json' => #{result => <<"OK">>},
+    'application/xml' => <<"<Message> OK </Message>">>,
+    'random/nonsense' => <<"Some">>
+  },
+  case Req of
+    #{response_view := <<"simple">>} -> maps:get(ContentType, Content);
+    _ -> {raw, 200, #{<<"content-type">> => atom_to_binary(ContentType)}, maps:get(ContentType, Content)} 
+  end.
