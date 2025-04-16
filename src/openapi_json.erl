9e643fa4f8848209a2b513bd5b5298e4f5936508
Danil Zagoskin <dzagoskin@usergate.com>
2025-04-16 08:07:05 +0300

    openapi_json: use native json on OTP 27+

--- src/openapi_json.erl
+++ src/openapi_json.erl
@@ -3,6 +3,31 @@
 -export([encode/1]).
 -export([decode/1, decode_with_atoms/1]).
 
+-if(?OTP_RELEASE >= 27).
+
+decode(Bin) ->
+  try
+    json:decode(Bin)
+  catch
+    _:_:_ -> {error, broken_json}
+  end.
+
+json_push_atom(Key, Value, Acc) ->
+  [{binary_to_atom(Key), Value} | Acc].
+
+decode_with_atoms(Bin) ->
+  {Object, ok, <<>>} = json:decode(Bin, ok, #{object_push => fun json_push_atom/3}),
+  Object.
+
+encode(Source) ->
+  try
+    iolist_to_binary(json:encode(Source))
+  catch
+    _:_:_ -> iolist_to_binary(json:encode(#{error => error_in_json_encoder, input => iolist_to_binary(io_lib:format("~p",[Source]))}))
+  end.
+
+-else. %% ?OTP_RELEASE < 27
+
 decode(Bin) ->
   try jsx:decode(Bin,[return_maps])
   catch
@@ -12,12 +37,10 @@ decode(Bin) ->
 decode_with_atoms(Bin) ->
   jsx:decode(Bin,[return_maps,{labels,atom}]).
 
-
-
-
 encode(Source) ->
   try jsx:encode(Source)
   catch
     _:_:_ -> jsx:encode(#{error => error_in_json_encoder, input => iolist_to_binary(io_lib:format("~p",[Source]))})
   end.
 
+-endif. %% ?OTP_RELEASE
